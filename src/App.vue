97437a3640490e08360ed4577a3c19827fb7e5c2
zihan7 <zihan7@staff.sina.com.cn>
2019-03-18 17:07:11 +0800

    update header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>Welcome to Vue.js-Study</h1>
+  <div id="wrap">
+    <TheHeader/>
+    <TheFooter/>
   </div>
 </template>
 
 <script>
+import TheHeader from '@/components/layouts/TheHeader'
+// 引入 TheFooter.vue 的默认值
+import TheFooter from '@/components/layouts/TheFooter'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TheHeader,
+    // 注册 TheFooter
+    TheFooter
+  }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+$container-large-desktop: 1200px;
+$btn-primary-bg: #f7732e;
+$btn-primary-border: #f7732e;
+$label-primary-bg: #f7732e;
+$pagination-active-bg: #f7732e;
+$pagination-active-border: #f7732e;
+$pagination-color: #f7732e;
+$input-border-focus: #f7732e;
+$link-color: #12c4c5;
+$link-hover-color: #22ddde;
+$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+$fa-font-path: "~font-awesome/fonts/";
+
+@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+@import "~font-awesome/scss/font-awesome";
+@import "./styles/main";
+@import "./styles/extra";
 </style>
